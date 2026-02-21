5768c9ff868d1a923de1895ed1f4be09e5dfebd5
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-02-21 10:44:17 +0600

    create cashout interfaec and connect it to cashout js file

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById('withdraw-btn').addEventListener("click", function () {
+  // 1. get the ageent number & validate
+  // 2. get the amount , validate , convert to number
+  // 3. get the current balace , validate , convert to number
+  // 4. calculate the new balace
+  // 5. get the pin and verify
+  // 5-1. if true show cashout succesfull massage and ser new balace
+  // 5-2. else false show eror massage and return 
+})
